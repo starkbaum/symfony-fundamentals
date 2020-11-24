60771e19069de68511c8d0df4e6aad39c8d09bc3
starkbaum <s.starkbaum@austrian-standards.at>
2020-11-24 17:40:17 +0100

    Chapter 15: named autowiring

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -5,6 +5,7 @@ namespace App\Service;
 
 
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 
 class MarkdownHelper
@@ -12,16 +13,22 @@ class MarkdownHelper
     private $markdownParser;
     private $cache;
     private $isDebug;
+    private $logger;
 
-    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache, bool $isDebug)
+    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache, bool $isDebug, LoggerInterface $markdownLogger)
     {
         $this->markdownParser = $markdownParser;
         $this->cache = $cache;
         $this->isDebug = $isDebug;
+        $this->logger = $markdownLogger;
     }
 
     public function parse(string $source): string
     {
+        if (stripos($source, 'cat') !== false) {
+            $this->logger->info('Meow!');
+        }
+
         if ($this->isDebug) {
             return $this->markdownParser->transformMarkdown($source);
         }
